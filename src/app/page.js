747e8e1c48060b6e775299c5c7e14d3e470a6175
Sanjeev Rai <srai089@gmail.com>
2023-11-29 12:41:51 +0530

    Add Image

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,11 @@
 import Image from 'next/image'
-import styles from './page.module.css'
+import styles from './page.module.css';
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <h1>Deploy Next js project on vercel</h1>
+      <Image src="/pic.jpg" alt='img' width={300} height={200}/>
       <p>this project is developed for testing purpose</p>
     </main>
   )
