19af71769d0e54e7d5f1c0f1feb5d1e27e191b8c
Sanjeev Rai <srai089@gmail.com>
2023-11-29 11:55:48 +0530

    creating navbar

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from 'next/font/google'
-import './globals.css'
+import './globals.css';
+import Link from 'next/link';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,7 +12,16 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className='navbar'>
+        <ul>
+          <li> <Link href="/">Home</Link> </li>
+          <li> <Link href="/menu1">Menu1</Link> </li>
+          <li> <Link href="/menu2">Menu2</Link> </li>
+        </ul>
+        </div>
+        
+        {children}</body>
     </html>
   )
 }
---
+++ src/app/menu1/page.js
@@ -0,0 +1,7 @@
+export default function Page(){
+    return(
+        <div>
+            <h1>This is Menu one</h1>
+        </div>
+    )
+}
